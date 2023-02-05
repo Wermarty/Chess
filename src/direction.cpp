e44d35abf3d3a3efb6e5b7b581a7f60806c64327
Wermarty <112768311+Wermarty@users.noreply.github.com>
2023-02-05 20:12:35 +0100

    Possible direction for rook/bishop/queen

---
+++ src/direction.cpp
@@ -0,0 +1,19 @@
+#include "direction.h"
+
+
+Direction::Direction(Dir dir) {
+	switch (dir) {
+	case Dir::Up:
+		x = -1; y = 0;
+		break;
+	case Dir::Down:
+		x = 1; y = 0;
+		break;
+	case Dir::Left:
+		x = 0; y = -1;
+		break;
+	case Dir::Right:
+		x = 0; y = 1;
+		break;
+	}
+}
